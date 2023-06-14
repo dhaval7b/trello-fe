b5c585a8dd23e673282987c163df830c4f44cf77
dhaval-b <109001593+dhaval7b@users.noreply.github.com>
2023-06-14 15:03:47 -0400

    fix: code fix

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -4,21 +4,23 @@ import { RouterModule, Routes } from "@angular/router";
 import { AuthService } from './auth.service';
 import { RegisterComponent } from './register/register.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login/login.component';
 
 const routes : Routes = [{
   path: "register",
   component: RegisterComponent,
 },
-// {
-//   path: "login",
-//   component: RegisterComponent
-// }
+{
+  path: "login",
+  component: LoginComponent
+}
 
 ];
 
 @NgModule({
   declarations: [
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent
   ],
   imports: [
     ReactiveFormsModule,
