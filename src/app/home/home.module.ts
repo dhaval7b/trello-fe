51b4f1d2af1fce8916fdb6f517059e225ab6412f
dhaval7b <dhavalrbhimani@gmail.com>
2023-06-18 11:00:41 -0400

    fix: added home route

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path : "",
+    component: HomeComponent
+  
+  }
+]
 
 @NgModule({
   declarations: [
@@ -11,7 +17,7 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule.forChild(routes)
   ]
 })
 export class HomeModule { }
